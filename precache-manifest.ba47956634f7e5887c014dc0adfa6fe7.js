b262c2719818a383d57a6d28fdf4765d071dc9a4
raphaelbrodrigues <raphaelbarbosa.rodrigues@gmail.com>
2020-08-09 17:21:36 -0300

    Updates

---
+++ precache-manifest.ba47956634f7e5887c014dc0adfa6fe7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eace94e6dc20b0dec563d56f4dde323d",
+    "url": "/react-cv/index.html"
+  },
+  {
+    "revision": "58efa8cdb48c0d5f03e2",
+    "url": "/react-cv/static/css/main.54f4ab25.chunk.css"
+  },
+  {
+    "revision": "b03d209a14d837a8c9b5",
+    "url": "/react-cv/static/js/2.f8046120.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-cv/static/js/2.f8046120.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58efa8cdb48c0d5f03e2",
+    "url": "/react-cv/static/js/main.960c286e.chunk.js"
+  },
+  {
+    "revision": "2f2887c6b05e189352ca",
+    "url": "/react-cv/static/js/runtime-main.20e19365.js"
+  },
+  {
+    "revision": "d19bbdb4c119b7d984414179907a58d1",
+    "url": "/react-cv/static/media/git.d19bbdb4.png"
+  },
+  {
+    "revision": "57de3b6ce4924d85ff2204fcb8c4cf66",
+    "url": "/react-cv/static/media/logo.57de3b6c.svg"
+  }
+]);
