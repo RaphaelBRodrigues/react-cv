fa7ea2ef3fe0e34107ba398602ad21bcf5734ba9
raphaelbrodrigues <raphaelbarbosa.rodrigues@gmail.com>
2020-10-05 21:24:46 -0300

    Updates

--- precache-manifest.29afedc8cc494396d4cc353cab233fe6.js
+++ precache-manifest.29afedc8cc494396d4cc353cab233fe6.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "952f266550c7f02fafd2a2d3c8602aa1",
+    "revision": "b1b50c22663d07d9bac57bff3f987023",
     "url": "/react-cv/index.html"
   },
   {
-    "revision": "6522e2bc0248c626d0d2",
-    "url": "/react-cv/static/css/main.a5a1295c.chunk.css"
+    "revision": "31db4caefeebea5fa671",
+    "url": "/react-cv/static/css/main.2c593db6.chunk.css"
   },
   {
-    "revision": "b657860c766cba8ae3a3",
-    "url": "/react-cv/static/js/2.0cc8991f.chunk.js"
+    "revision": "0457d3b60cf7d1b887c0",
+    "url": "/react-cv/static/js/2.fba4f590.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/react-cv/static/js/2.0cc8991f.chunk.js.LICENSE.txt"
+    "url": "/react-cv/static/js/2.fba4f590.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6522e2bc0248c626d0d2",
-    "url": "/react-cv/static/js/main.2920575e.chunk.js"
+    "revision": "31db4caefeebea5fa671",
+    "url": "/react-cv/static/js/main.16010463.chunk.js"
   },
   {
     "revision": "2f2887c6b05e189352ca",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-cv/precache-manifest.1f259717f77f3947d3840ed8cdef3872.js"
+  "/react-cv/precache-manifest.29afedc8cc494396d4cc353cab233fe6.js"
 );
 
 self.addEventListener('message', (event) => {
